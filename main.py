b7f2d8d6fdcffff26e53a851800ab23dea941197
friedballerina <aleksandra.ikonnikova@gmail.com>
2022-12-07 16:15:46 +0000

    implemented Game class

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from time import sleep
 import random
 from hangman_parts import parts
 
+class Game:
+    def __init__(self, word):
+        self.word = word
+        self.guesses_left = 6
+        self.right_guesses = ['_'] * len(word) 
+        self.wrong_guesses = [] 
+
 #Prints a hangman picture
 print("Welcome to Hangman")
 print('   ',  '------')
@@ -19,13 +26,12 @@ print('Let me think of a word')
 def wait():
     for i in range(5):
         print('.', end = "")
-        sleep(.5)
+        sleep(.2)
     print()
     
 wait()
 
 #List of words
-
 file = open("words.txt", "r")
   
 # reading the file
@@ -35,78 +41,77 @@ data = file.read()
 # when newline ('\n') is seen.
 words = data.split("\n")
 file.close()
-        
 
-picked = random.choice(words)
+# creating an instance of a Game class  
+game = Game(random.choice(words)) 
 
-print("Okay Ive got it!")
+print("Okay I've got it!")
 
-print("The word has", len(picked), "letters")
+print("The word has", len(game.word), "letters")
 
-for i in range(len(picked)):
+for i in range(len(game.word)):
     print('_',' ', end = "")
     
 print()
 
-
-#Creates a list with _ marks for letters
-right = ['_'] * len(picked) 
-wrong = []
-
 #Prints letters in right with _'s
 def add_letter():
-    for i in right:
+    for i in game.right_guesses:
         print(i, ' ', end = "")
     print()
 
 #Prints out wrong letters
 def wrong_letter():
     print("Wrong letters: ", end = "")
-    for i in wrong:
+    for i in game.wrong_guesses:
         print(i,' ',end = "")
     print()
 
 #Main Loop        
 while True:
     print('=====================')
+    if game.guesses_left > 1:
+        print(game.guesses_left, "guesses left.")
+    else:
+        print(game.guesses_left, "guess left")
     guess = input("Guess a letter: ")
-    if guess in picked:
+    if guess in game.word:
         print("Let me check")
 
         ###Handles letter that appear more than once
-        
         index = 0
-        for i in picked:
+        for i in game.word:
             if i == guess:
-                right[index] = guess
+                game.right_guesses[index] = guess
             index = index + 1
 
         wait()
         add_letter()
         wrong_letter()
-        parts(len(wrong))
+        parts(len(game.wrong_guesses))
         
-    elif guess not in picked:
+    elif guess not in game.word:
         print("Let me check")
         wait()
-        if guess in wrong:
+        if guess in game.wrong_guesses:
             print("You already guessed", guess)
             wrong_letter()
         else:
             print(guess, "is not in my word")
-            wrong.append(guess)
+            game.wrong_guesses.append(guess)
+            game.guesses_left -= 1
             add_letter()
             wrong_letter()
-            parts(len(wrong))
+            parts(len(game.wrong_guesses))
             
-    if len(wrong) >= 6:
+    if len(game.wrong_guesses) == 6:
         print("Game Over!")
-        print("I picked", picked)
+        print("I picked", game.word)
         break
     
-    if '_' not in right:
+    if '_' not in game.right_guesses:
         print("You guessed it!")
-        print("I picked", picked)
+        print("I picked", game.word)
         break
 
 # https://www.mrmichaelsclass.com/python-programming/python-projects/hangman
